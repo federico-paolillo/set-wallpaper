f12af7e9680a2986d8dc7a2a46b2c949eb64b123
federico-paolillo <31439756+federico-paolillo@users.noreply.github.com>
2020-04-09 17:00:45 +0200

    Add project files.

---
+++ SetWallpaper.COM/User32.cs
@@ -0,0 +1,13 @@
+﻿namespace SetWallpaper.COM
+{
+    public static class User32
+    {
+        /// <summary>
+        /// Does nothing and returns garbage.
+        /// </summary>
+        public static string Dummy()
+        {
+            return "Garbage";
+        }
+    }
+}
