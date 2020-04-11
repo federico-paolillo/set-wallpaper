3bb0210d21fd10bd226dfe5aa4295bce8843d26c
federico-paolillo <31439756+federico-paolillo@users.noreply.github.com>
2020-04-11 18:28:07 +0200

    Implemented Get-Monitors Cmdlet

---
+++ SetWallpaper.COM/IDesktopWallpaper.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace SetWallpaper.COM
+{
+    [ComImport, Guid("B92B56A9-8B55-4E14-9A89-0199BBB6F93B"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    public interface IDesktopWallpaper
+    {
+        [PreserveSig]
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        int SetWallpaper([MarshalAs(UnmanagedType.LPWStr), In] string monitorID, [MarshalAs(UnmanagedType.LPWStr), In] string wallpaper);
+
+        [PreserveSig]
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        int GetWallpaper([MarshalAs(UnmanagedType.LPWStr), In] string monitorID, [MarshalAs(UnmanagedType.LPWStr), Out] out string wallpaper);
+
+        [PreserveSig]
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        int GetMonitorDevicePathAt([MarshalAs(UnmanagedType.U4), In] uint monitorIndex, [MarshalAs(UnmanagedType.LPWStr), Out] out string monitorID);
+
+        [PreserveSig]
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        int GetMonitorDevicePathCount([MarshalAs(UnmanagedType.U4), Out] out uint count);
+    }
+}
---
+++ SetWallpaper/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace SetWallpaper
+{
+    public static class Extensions
+    {
+        public static void Release(this object comObject)
+        {
+            if (comObject == null) return;
+
+            while (Marshal.ReleaseComObject(comObject) > 0) { };
+        }
+    }
+}
